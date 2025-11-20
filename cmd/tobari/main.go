16c8049d4dfcbcd1df26450f5af0a9a360925c54
Masaaki Goshima <goccy54@gmail.com>
2025-11-20 19:32:44 +0900

    add comment

--- cmd/tobari/main.go
+++ cmd/tobari/main.go
@@ -70,10 +70,21 @@ func filterCoveragecfg(args []string) ([]string, error) {
 	return ret, nil
 }
 
+// Normally, the path in the importcfg file uses the "$WORK" variable.
+// Since "$WORK" is not an environment variable and can only be resolved by the default Go tool, its actual path is usually unknown.
+// However, when the -cover option is enabled, it is a rule that the path to the -coveragecfg option is passed at compile time, and that path is already resolved.
+// Furthermore, there is also a rule that both the coveragecfg and importcfg files are written to the same directory.
+// Therefore, it is possible to deduce the path to the importcfg file from the path to the coveragecfg file.
 func detectImportcfgPath(coveragecfgPath string) string {
 	return filepath.Join(filepath.Dir(coveragecfgPath), "importcfg")
 }
 
+// In Tobari, the AST of the files targeted for coverage measurement is manipulated,
+// and the GID() and PGID() functions—dynamically added to the runtime package using the overlay option—are used to measure the Goroutine ID.
+// However, if the original file being measured does not import the runtime package, the importcfg file will not contain an archive file path entry for the runtime package,
+// resulting in a compilation failure due to unresolved references to the runtime package.
+// To prevent this, a reference to the runtime package is dynamically added to the importcfg file.
+// Since the runtime package is always built in advance, its archive file path is stored and used to resolve the reference.
 func addRuntimePkgIfNeeded(importcfgPath string) error {
 	importcfg, err := os.ReadFile(importcfgPath)
 	if err != nil {
@@ -107,6 +118,8 @@ func isCoverVOption(args []string) bool {
 	return len(args) == 1 && strings.HasPrefix(args[0], "-V")
 }
 
+// The path from when the runtime package was built is saved and later used when adding it to the importcfg.
+// Currently, a single temporary path is used for storing this value, which can lead to race conditions if multiple build processes run concurrently.
 func saveArchiveFilePathIfRuntimePkgCompilation(toolName string, toolArgs []string) error {
 	if toolName != "compile" {
 		return nil
